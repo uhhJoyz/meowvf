93aadfb302f39685586f488f768a14a9b513431a
uhhJoyz <wcb8ze@virginia.edu>
2026-01-01 13:58:00 -0500

    removed file tree open on launch

--- modules/plugins/filetree/nvimtree/config.nix
+++ modules/plugins/filetree/nvimtree/config.nix
@@ -59,54 +59,6 @@ in {
             end,
           })
         ''}
-
-        ${
-          optionalString cfg.openOnSetup ''
-            ${optionalString config.vim.lazy.enable ''require('lz.n').trigger_load("nvim-tree-lua")''}
-            -- autostart behaviour
-            -- Open on startup has been deprecated
-            -- see https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup
-
-            -- use a nix eval to dynamically insert the open on startup function
-            local function open_nvim_tree(data)
-              local IGNORED_FT = {
-                "markdown",
-              }
-
-              -- buffer is a real file on the disk
-              local real_file = vim.fn.filereadable(data.file) == 1
-
-              -- buffer is a directory
-              local directory = vim.fn.isdirectory(data.file) == 1
-
-              -- buffer is a [No Name]
-                local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
-
-              -- &ft
-              local filetype = vim.bo[data.buf].ft
-
-              -- only files please
-              if not real_file and not directory and not no_name then
-                return
-              end
-
-              -- skip ignored filetypes
-              if vim.tbl_contains(IGNORED_FT, filetype) then
-                return
-              end
-
-              -- cd if buffer is a directory
-              if directory then
-                vim.cmd.cd(data.file)
-              end
-              -- open the tree but don't focus it
-              require("nvim-tree.api").tree.toggle({ focus = false })
-            end
-
-            -- function to automatically open the tree on VimEnter
-            vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-          ''
-        }
       '';
     };
   };
