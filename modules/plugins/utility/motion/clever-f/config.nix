a69e3772f8926b807db186be0c3f68e74bbe61f9
uhhJoyz <wcb8ze@virginia.edu>
2026-01-01 21:54:57 -0500

    update

--- modules/plugins/utility/motion/clever-f/config.nix
+++ modules/plugins/utility/motion/clever-f/config.nix
@@ -13,15 +13,22 @@ in
   config = mkIf cfg.enable {
     vim = {
       lazy.plugins = {
-        "clever-f" = {
-          package = "clever-f";
+        "clever-f.vim" = {
+          package = "clever-f.vim";
           lazy = true;
           event = [ "BufEnter" ];
+          keys = [
+            {
+              mode = "n";
+              key = ";";
+              action = "<Plug>";
+            }
+          ];
           before = ''
             vim.g.clever_f_show_prompt = 1
             vim.g.clever_f_fix_key_direction = 1
             vim.g.clever_f_smart_case = 1
-            vim.g.clever_f_chars_match_any_signs = ";,"
+            vim.g.clever_f_chars_match_any_signs = ","
             vim.g.clever_f_mark_char_color = "@text.strong"
           '';
         };
